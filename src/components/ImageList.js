fd9d025fa94e029e71bc82c005e8b7a4d67355b0
Marta Jankowska <martajan218@gmail.com>
2021-01-04 13:13:20 +0100

    little changes

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const ImageList = (props) => {
-  const images = props.images.map((image, key) => {
-    return <img key={key} src={image.urls.small} alt="result" />;
+  const images = props.images.map((image) => {
+    return <img key={image.id} src={image.urls.small} alt={image.description} />;
   });
 
   return <div>{images}</div>;

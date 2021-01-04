a48fb3dca4444e8e9f51d665cc95c6938600a0f1
Marta Jankowska <martajan218@gmail.com>
2021-01-04 12:39:29 +0100

    search with axios call working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 import SearchBar from "./components/SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: `Client-ID ${process.env.REACT_APP_ACCESS_KEY}`,
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
